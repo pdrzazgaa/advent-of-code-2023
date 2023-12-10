4147ff5d078e19b319ba48572b85b6d89659afbc
Paulina Drzazga <paulina@halpress.team>
2023-12-10 22:34:47 +0100

    Resolved task 2

---
+++ day-3/src/Coordinates.java
@@ -0,0 +1,16 @@
+import java.util.Objects;
+
+public record Coordinates(int row, int column) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return row == that.row && column == that.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+}
--- day-3/src/Main.java
+++ day-3/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
+import java.security.KeyPair;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -20,7 +22,7 @@ public class Main {
             List<String> row = charsMatrix.get(rowIndex);
             for (int charIndex = 0; charIndex < charsMatrix.size(); charIndex++){
                 try {
-                    int num = Integer.parseInt(row.get(charIndex));
+                    Integer.parseInt(row.get(charIndex));
                     List<Integer> indexes = getNumbersIndexes(row, charIndex);
                     boolean isComponent = false;
                     for (int index:indexes) {
@@ -75,9 +77,81 @@ public class Main {
         return isDigitConnected;
     }
 
+    public int resolveTask2(){
+        int sum = 0;
+        HashMap<Coordinates, List<Integer>> starsNumbers = new HashMap<>();
+        HashMap<Coordinates, Integer> starsAmount = new HashMap<>();
+        for (int rowIndex = 0; rowIndex < charsMatrix.size(); rowIndex++){
+            List<String> row = charsMatrix.get(rowIndex);
+            for (int charIndex = 0; charIndex < charsMatrix.size(); charIndex++){
+                try {
+                    Integer.parseInt(row.get(charIndex));
+                    List<Integer> indexes = getNumbersIndexes(row, charIndex);
+                    boolean isComponent = false;
+                    for (int index:indexes) {
+                        isComponent = isComponent | isStarConnectedWithTwoNumbers(rowIndex, index, starsAmount);
+                    }
+                    if (isComponent) {
+                        String strNumber = "";
+                        for (int index:indexes) {
+                            strNumber += row.get(index);
+                            charIndex = index;
+                        }
+                        int number = Integer.parseInt(strNumber);
+                        for (Coordinates coordinates : starsAmount.keySet()) {
+                            if (starsNumbers.containsKey(coordinates)) {
+                                starsNumbers.get(coordinates).add(number);
+                                starsAmount.remove(coordinates);
+                            }
+                            else {
+                                List<Integer> list = new ArrayList<>();
+                                list.add(number);
+                                starsNumbers.put(coordinates, list);
+                                starsAmount.remove(coordinates);
+                            }
+                        }
+                    }
+                } catch (NumberFormatException ignored) {}
+            }
+        }
+
+        for (Coordinates coordinates: starsNumbers.keySet()){
+            int multiplication = 1;
+            if (starsNumbers.get(coordinates).size() == 2){
+                for (int num : starsNumbers.get(coordinates))
+                    multiplication*=num;
+                sum += multiplication;
+            }
+        }
+        return sum;
+    }
+
+    private boolean isStarConnectedWithTwoNumbers(int row, int column, HashMap<Coordinates, Integer> starsAmount){
+        int top, bottom, left, right;
+        if (row != charsMatrix.size()-1) bottom = row + 1; else bottom = row;
+        if (row != 0) top = row - 1; else top = row;
+        if (column != charsMatrix.get(row).size() - 1) right = column + 1; else right = column;
+        if (column != 0) left = column - 1; else left = column;
+        boolean isNextToStar = false;
+        for (int i = top; i <= bottom; i++){
+            for (int j = left; j <= right; j++){
+                if (i != row || j != column) {
+                    if (charsMatrix.get(i).get(j).equals("*")) {
+                        Coordinates coordinates = new Coordinates(i, j);
+                        starsAmount.put(coordinates, 1);
+                        isNextToStar = true;
+                    }
+                }
+            }
+        }
+        return isNextToStar;
+    }
+
     public static void main(String[] args) throws IOException {
         Main program = new Main();
         program.readData();
         System.out.println(program.resolveTask1());
+        // 84266818
+        System.out.println(program.resolveTask2());
     }
 }
