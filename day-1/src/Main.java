b12296eb96f41fc0f7be5ac8f9fb35cf32bcc3da
Paulina Drzazga <paulina@halpress.team>
2023-12-10 15:09:10 +0100

    Resolved task 2

--- day-1/src/Main.java
+++ day-1/src/Main.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 public class Main {
     ArrayList<String> dataList = new ArrayList<>();
-    public void readData() throws IOException{
+
+    public void readData() throws IOException {
         File file = new File("input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
@@ -11,30 +12,56 @@ public class Main {
             dataList.add(st);
     }
 
-    public int resolveTask1(){
+
+    private int resolveTask(boolean isTask2) {
         int sum = 0;
 
-        for (String row: dataList){
+        for (String row : dataList) {
             ArrayList<String> numbersInRow = new ArrayList<>();
+            if (isTask2) row = detectDigitsFromString(row);
             String[] chars = row.split("");
-            for (String c: chars){
+            for (String c : chars) {
                 try {
                     int num = Integer.parseInt(c);
                     numbersInRow.add(String.valueOf(num));
-                } catch (NumberFormatException ignored) {}
+                } catch (NumberFormatException ignored) {
+                }
             }
 
             String firstValue = numbersInRow.getFirst();
             String lastValue = numbersInRow.getLast();
-            sum += Integer.parseInt(firstValue+lastValue);
+            sum += Integer.parseInt(firstValue + lastValue);
         }
 
         return sum;
     }
 
+    private int resolveTask1() {
+        return resolveTask(false);
+    }
+
+    private int resolveTask2() {
+        return resolveTask(true);
+    }
+
+    private String detectDigitsFromString(String digitRow) {
+        digitRow = digitRow.replaceAll("one", "o1ne");
+        digitRow = digitRow.replaceAll("two", "tw2o");
+        digitRow = digitRow.replaceAll("three", "thr3ee");
+        digitRow = digitRow.replaceAll("four", "fo4ur");
+        digitRow = digitRow.replaceAll("five", "fi5ve");
+        digitRow = digitRow.replaceAll("six", "si6x");
+        digitRow = digitRow.replaceAll("seven", "se7ven");
+        digitRow = digitRow.replaceAll("eight", "ei8ght");
+        digitRow = digitRow.replaceAll("nine", "ni9ne");
+        return digitRow;
+    }
+
+
     public static void main(String[] args) throws IOException {
-         Main program = new Main();
-         program.readData();
+        Main program = new Main();
+        program.readData();
         System.out.println(program.resolveTask1());
+        System.out.println(program.resolveTask2());
     }
 }
