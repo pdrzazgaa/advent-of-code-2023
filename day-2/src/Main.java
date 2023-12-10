18eda042ad737ba3ea9d3f11fd5b15198a33d7d2
Paulina Drzazga <paulina@halpress.team>
2023-12-10 17:34:00 +0100

    Resolved day 2

--- day-2/src/Main.java
+++ day-2/src/Main.java
@@ -1,17 +1,88 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    HashMap<Integer, ArrayList<ArrayList<HashMap<String,Integer>>>> data = new HashMap<>();
+    HashMap<String, Integer> colorsAmount;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public Main() {
+        colorsAmount = new HashMap<>();
+        colorsAmount.put("blue", 14);
+        colorsAmount.put("green", 13);
+        colorsAmount.put("red", 12);
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public void readData() throws IOException {
+        File file = new File("input.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        String[] t1, t2, t3;
+        ArrayList<ArrayList<HashMap<String,Integer>>> roundsInGame;
+        ArrayList<HashMap<String,Integer>> turnsInRound;
+        while ((st = br.readLine()) != null){
+            st = st.replaceAll("Game ", "");
+            t1 = st.split(": ");
+            int gameIndex = Integer.parseInt(t1[0]);
+            roundsInGame = new ArrayList<>();
+            t1 = t1[1].split("; ");
+            for (String round : t1){
+                turnsInRound = new ArrayList<>();
+                t2 = round.split(", ");
+                for (String turn: t2){
+                    t3 = turn.split(" ");
+                    HashMap<String, Integer> map = new HashMap<>();
+                    map.put(t3[1], Integer.parseInt(t3[0]));
+                    turnsInRound.add(map);
+                }
+                roundsInGame.add(turnsInRound);
+            }
+            data.put(gameIndex, roundsInGame);
         }
+    }
+
+    public int resolveTask1(){
+        int sum = 0;
+        for (int game: data.keySet()){
+            boolean isPlayable = true;
+            for (ArrayList<HashMap<String,Integer>> round: data.get(game)){
+                for (HashMap<String,Integer> turn: round){
+                    if (turn.get("red") != null && colorsAmount.get("red") < turn.get("red")) isPlayable = false;
+                    if (turn.get("blue") != null && colorsAmount.get("blue") < turn.get("blue")) isPlayable = false;
+                    if (turn.get("green") != null && colorsAmount.get("green") < turn.get("green")) isPlayable = false;
+                }
+            }
+            if (isPlayable) sum+=game;
+        }
+        return sum;
+    }
+
+    public int resolveTask2(){
+        int sum = 0;
+        for (int game: data.keySet()){
+            HashMap<String, Integer> minSets = new HashMap<>();
+            minSets.put("red", 0);
+            minSets.put("green", 0);
+            minSets.put("blue", 0);
+            for (ArrayList<HashMap<String,Integer>> round: data.get(game)){
+                for (HashMap<String,Integer> turn: round){
+                    if (turn.get("red") != null && minSets.get("red") < turn.get("red")) minSets.put("red", turn.get("red"));
+                    if (turn.get("blue") != null && minSets.get("blue") < turn.get("blue")) minSets.put("blue", turn.get("blue"));
+                    if (turn.get("green") != null && minSets.get("green") < turn.get("green")) minSets.put("green", turn.get("green"));
+                }
+            }
+            sum+=minSets.get("red")*minSets.get("green")*minSets.get("blue");
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main program = new Main();
+        program.readData();
+        System.out.println(program.resolveTask1());
+        System.out.println(program.resolveTask2());
     }
 }
